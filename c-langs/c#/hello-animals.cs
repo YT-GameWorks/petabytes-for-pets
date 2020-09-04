ef62fa413b4453b50a861c5a15829994c8901b67
ExceptionalPikachu <70340100+ExceptionalPikachu@users.noreply.github.com>
2020-09-04 01:06:48 -0400

    Create hello-animals.cs

---
+++ c-langs/c#/hello-animals.cs
@@ -0,0 +1,12 @@
+using System
+
+namespace HelloAnimals
+{
+    class StopAnimalAbuse {         
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello animals!");
+            Console.WriteLine("Stop animal abuse");
+        }
+    }
+}
