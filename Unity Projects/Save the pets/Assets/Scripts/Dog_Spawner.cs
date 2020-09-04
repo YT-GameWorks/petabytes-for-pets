ed6c26ee78d365d4ae26a63fb655f5e0204c5497
SOHAMbuban <sinha.soham@outlook.com>
2020-09-04 14:52:21 +0530

    Added more art and spawner code

---
+++ Unity Projects/Save the pets/Assets/Scripts/Dog_Spawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dog_Spawner : MonoBehaviour
+{
+    public float SpawnRate = 1f;
+
+    public GameObject DogPrefab;
+
+    private float NextTimeToSpawn;
+
+    private void Update()
+    {
+        if (Time.time >= NextTimeToSpawn)
+        {
+            Instantiate(DogPrefab, Vector3.zero, Quaternion.identity);
+            NextTimeToSpawn = Time.time + 1f / SpawnRate;
+        }
+    }
+}
